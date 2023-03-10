372fea5879ea9c88b2dbd397fcf304b8cb81dec7
Raghida Nweiran <116009953+RaghidaN@users.noreply.github.com>
2023-03-10 11:48:03 -0500

    Removed comments

--- src/db/migrations/20230310100113_createCriticsTable.js
+++ src/db/migrations/20230310100113_createCriticsTable.js
@@ -14,10 +14,3 @@ exports.up = function (knex) {
   };
   
   
-
-//   The `critics` table represents movie critics who have created reviews for movies. Each critic has the following fields:
-
-// - `critic_id`: (Primary Key) A unique ID for the critic.
-// - `preferred_name`: (String) The critic's preferred first name.
-// - `surname`: (String) The critic's last name.
-// - `organization_name`: (String) The name of the organization the critic works for.
