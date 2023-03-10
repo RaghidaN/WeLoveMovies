79a98f0392db622c240229da75d8bfeae43e1516
Raghida Nweiran <116009953+RaghidaN@users.noreply.github.com>
2023-03-10 11:47:44 -0500

    Removed comments

--- src/db/migrations/20230310090215_createMoviesTable.js
+++ src/db/migrations/20230310090215_createMoviesTable.js
@@ -14,9 +14,3 @@ exports.up = function (knex) {
     return knex.schema.dropTable("movies");
   };
   
-// - `movie_id`: (Primary Key) A unique ID for the movie.
-// - `title`: (String) The title of the movie.
-// - `runtime_in_minutes`: (Integer) The length of the movie in minutes.
-// - `rating`: (String) The rating given to the movie.
-// - `description`: (Text) A shortened description of the movie.
-// - `image_url`: (String) A URL to the movie's poster.
