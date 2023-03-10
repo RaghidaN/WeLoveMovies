53146ad2653b25a316859baa6ea510c3702f307e
Raghida Nweiran <116009953+RaghidaN@users.noreply.github.com>
2023-03-10 11:48:45 -0500

    Removed comments

--- src/db/migrations/20230310100251_createMovies_TheatersTable.js
+++ src/db/migrations/20230310100251_createMovies_TheatersTable.js
@@ -20,10 +20,3 @@ exports.up = function (knex) {
   exports.down = function (knex) {
     return knex.schema.dropTable("movies_theaters");
   };
-
-
-// The `movies_theaters` table is a join table that connects movies with theaters. It represents which movies are being shown in which theaters. It also includes a key that represents whether or not a movie is currently showing at the theater, or if it has in the past.
-
-// - `movie_id`: (Foreign Key) A reference ID to a particular movie.
-// - `theater_id`: (Foreign Key) A reference ID to a particular theater.
-// - `is_showing`: (Boolean) A representation of whether or not the movie is currently showing in the referenced theater.
